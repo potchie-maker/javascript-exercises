270e48d6d1ab6cd74edb4616da4e47495e6ae3d1
potchie-maker <177373729+potchie-maker@users.noreply.github.com>
2024-11-23 23:09:25 -0500

    Pass first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, rem) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === rem) {
+      arr.splice(i, 1);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
