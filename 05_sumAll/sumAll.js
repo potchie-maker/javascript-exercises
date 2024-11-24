42797579a75de070577bf8688c20e556c59e92a5
potchie-maker <177373729+potchie-maker@users.noreply.github.com>
2024-11-24 10:16:23 -0500

    Pass rest of tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,16 @@
 const sumAll = function(num1, num2) {
   let sum = 0;
-  for (i = num1; i <= num2; i++) {
-    sum += i;
+  if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+    return "ERROR";
+  }else if (num1 < num2) {
+      for (i = num1; i <= num2; i++) {
+        sum += i;
+      }
+  }else if (num1 > num2) {
+    [num1, num2] = [num2, num1];
+    for (i = num1; i <= num2; i++) {
+      sum += i;
+    }
   }
   return sum;
 };
