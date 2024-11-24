6e5c19c48ab63255becea1786fa76d9a7cb5c456
potchie-maker <177373729+potchie-maker@users.noreply.github.com>
2024-11-24 18:20:52 -0500

    Pass rest of tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const convertToCelsius = function(fah) {
   let temp = (fah - 32) * (5/9);
-  return temp;
+  return Math.round(temp * 10) / 10;
 };
 
 const convertToFahrenheit = function(cel) {
   let temp = cel * (9/5) + 32;
-  return temp;
+  return Math.round(temp * 10) / 10;
 };
 
 // Do not edit below this line
