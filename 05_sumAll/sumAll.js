08acc7188a2fdaaea196e20a01776a342f0a1865
potchie-maker <177373729+potchie-maker@users.noreply.github.com>
2024-11-24 10:00:16 -0500

    Pass first test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  let sum = 0;
+  for (i = num1; i <= num2; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
