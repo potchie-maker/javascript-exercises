0539c9beec26ae576cde0cc4d12fda00c4c97f44
potchie-maker <177373729+potchie-maker@users.noreply.github.com>
2024-11-23 22:37:42 -0500

    Adjust formatting

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -4,7 +4,7 @@ const repeatString = function(string, num) {
   if (num < 0) result = "ERROR";
 
   for (let i = 0; i < num; i++) {
-      result += string;
+    result += string;
   }
   return result;
 };
