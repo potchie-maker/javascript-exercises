f2be23bd24b8e4587e908ef83458bcdab34589da
potchie-maker <177373729+potchie-maker@users.noreply.github.com>
2024-12-19 14:57:37 -0500

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArr) {
+	const result = numArr.reduce((sum, num) => sum + num, 0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(numArr) {
+  const result = numArr.reduce((product, num) => product * num, 1);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let numList = [];
+  for (let i = 0; i < num; i++) {
+    numList[i] = i + 1;
+  }
+  const result = numList.reduce((product, num) => product * num, 1);
+  return result;
 };
 
 // Do not edit below this line
