ed5f75c21796899662a272339a9498dd583f4951
potchie-maker <177373729+potchie-maker@users.noreply.github.com>
2024-12-19 23:46:15 -0500

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const lower = string.toLowerCase().replace(/[^a-z0-9]/g, '');
+  const reversed = lower.split('').reverse().join('');
+  console.log(lower);
+  console.log(reversed);
+  return lower === reversed;
 };
 
 // Do not edit below this line
