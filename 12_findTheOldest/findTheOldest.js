cb982c7bc98b76e64c9f7a2865e61abc545344aa
potchie-maker <177373729+potchie-maker@users.noreply.github.com>
2024-12-20 02:13:13 -0500

    Complete 12_find

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+ return people
+    .map((person) => ({
+      ...person,
+      yearOfDeath: person.yearOfDeath || new Date().getFullYear(),
+    }))
+    .sort((personA, personB) => (personB.yearOfDeath - personB.yearOfBirth) - (personA.yearOfDeath - personA.yearOfBirth))
+    [0];
 };
 
 // Do not edit below this line
