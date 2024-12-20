c770997354a8625ee45f64843578063a6910342c
potchie-maker <177373729+potchie-maker@users.noreply.github.com>
2024-12-20 01:21:23 -0500

    Complete 10_fibinacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  seqNum = Number(num);
+  
+  if (seqNum < 0) return 'OOPS';
+  if (seqNum === 0) return 0;
 
+  let fib = [1, 1];
+  
+  for (let i = 2; i < seqNum; i++) {
+    fib.push(fib[i - 1] + fib[i - 2]);
+  }
+  console.log(fib);
+
+  return fib[seqNum - 1];
 };
 
 // Do not edit below this line
